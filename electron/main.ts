f18d1b3b760decb546a81f380bf1f19ee1be3e3f
kreiszistvan <kreiszdev@gmail.com>
2021-06-01 17:00:33 +0200

    first

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,9 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as isDev from 'electron-is-dev';
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as isDev from "electron-is-dev";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+} from "electron-devtools-installer";
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,12 +11,12 @@ function createWindow() {
     height: 600,
     webPreferences: {
       // contextIsolation: false,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
   if (isDev) {
-    win.loadURL('http://localhost:3000/index.html');
+    win.loadURL("http://localhost:3000/index.html");
   } else {
     // 'build/index.html'
     win.loadURL(`file://${__dirname}/../index.html`);
@@ -23,15 +25,17 @@ function createWindow() {
   // Hot Reloading
   if (isDev) {
     // 'node_modules/.bin/electronPath'
-    require('electron-reload')(__dirname, {
-      electron: path.join(__dirname,
-        '..',
-        '..',
-        'node_modules',
-        '.bin',
-        'electron' + (process.platform === "win32" ? ".cmd" : "")),
+    require("electron-reload")(__dirname, {
+      electron: path.join(
+        __dirname,
+        "..",
+        "..",
+        "node_modules",
+        ".bin",
+        "electron" + (process.platform === "win32" ? ".cmd" : "")
+      ),
       forceHardReset: true,
-      hardResetMethod: 'exit'
+      hardResetMethod: "exit",
     });
   }
 
@@ -44,18 +48,18 @@ app.whenReady().then(() => {
   // DevTools
   installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
+    .catch((err) => console.log("An error occurred: ", err));
 
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Currency, EUR } from "copdeck-scraper/dist/types";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h3>{EUR.symbol}</h3>
+
         <a
           className="App-link"
           href="https://reactjs.org"
