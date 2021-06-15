f0f0401eeea16a8bc52c87ec759b2bd9654db720
kreiszistvan <kreiszdev@gmail.com>
2021-06-15 12:50:20 +0200

    fixed discord link

--- src/Settings/SettingsTab.tsx
+++ src/Settings/SettingsTab.tsx
@@ -413,8 +413,8 @@ const SettingsTab = (prop: {
 				<div className="mt-5 mb-2 border border-gray-300"></div>
 				<div className="flex flex-col items-start">
 					<p className="text-base font-bold text-gray-600">
-						CopDeck Price Alerts will always stay free. To help us develop it and bring
-						you a lot more awesomeness (like our{' '}
+						CopDeck Price Alerts is and will always be free. To help us develop it and
+						bring you a lot more awesomeness (like our{' '}
 						<a
 							className="text-theme-blue border-transparent underline"
 							target="_blank"
@@ -447,16 +447,14 @@ const SettingsTab = (prop: {
 							Contact us!
 						</a>
 					</div>
-					<div className="mt-1 flex flex-nowrap items-center">
-						<h3 className="text-lg font-bold text-gray-600">Join us on Discord!</h3>
-
+					<div className="mt-3 flex flex-nowrap items-center">
 						<a
 							target="_blank"
 							href="https://discord.com/invite/cQh6VTvXas"
-							className="button-default text-theme-blue border-transparent underline"
+							className="button-default p-0 text-theme-blue border-transparent underline"
 							type="submit"
 						>
-							<img className="w-32" src="/discord.svg" alt="" />
+							Join us on Discord!
 						</a>
 					</div>
 				</div>
